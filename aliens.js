98dd8fd8f65bb63b6088949eade9b18192b91454
kylelingat <kylejlingat@gmail.com>
2016-10-24 08:09:50 -1000

    Add files via upload

---
+++ aliens.js
@@ -0,0 +1,18 @@
+var blue = 3000;
+var green = 3000;
+var red = 4000;
+
+var alien = blue + green + red;
+
+if (alien == 10000) {
+  console.log(alien);
+  if (green > 1000) {
+    console.log('We Gonna Die');
+  }
+  if (green + red > 2000) {
+    console.log('anything')
+  }
+  if (red + blue > 3000) {
+    console.log('ayy lmao')
+  }
+}
